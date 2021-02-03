1a338df65d9d88a4663b5cb7638c2baae5ac0c4f
Tom <tomwoodbridge32@gmail.com>
2021-02-03 09:51:19 +0000

    Week 4 started

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import sys
+import csv
 import os
 import proj_fun
 def clear():
     os.system( 'cls' )
+    
+    
+    #Working on writing products to csv file (Line 52 - 57)
 
 
 print('Welcome to the app!\n')
@@ -29,135 +33,151 @@ orders = [
     }
 ]
 # Opens products file and appends stored product names to empty product list above.
-with open("products.txt", "r") as products_file:
-    for product in products_file.readlines():
-        products.append(product.strip())
+with open("products.csv", 'r') as products_file:
+    products_csv = csv.DictReader(products_file)
+    for row in products_csv:
+        products.append(row)
 # Opens couriers file and appends stored courier names to empty courier list above.
 with open("couriers.txt", "r") as couriers_file:
     for courier in couriers_file.readlines():
         couriers.append(courier.strip())
 while True:
     print("Press... \n 0 to exit the app \n 1 to show Product Menu \n 2 to show Courier Menu \n 3 to show Order Menu")
-    value = int(input("Select a Main Menu option "))
+    str_value = (input("Select a Main Menu option "))
     clear()
-    if value == 0:
-        print("Thank you for using the app!")
-        with open("products.txt", "w") as product_file:
-            for product in products:
-                product_file.write(product + "\n")
-        with open("couriers.txt", "w") as couriers_file:
-            for courier in couriers:
-                couriers_file.write(courier + "\n")
-        sys.exit(0)
-    elif value == 1:
-        product_menu = True
-        # This keeps the user in the products menu until they choose to leave.
-        while product_menu == True:
-            print("Press... \n 0 to exit the app"
-                  + "\n 1 to see products" 
-                  + "\n 2 to add a new product" 
-                  + "\n 3 to update an existing product"
-                  + "\n 4 to delete a product"
-                  + "\n 5 to return to Main Menu\n")
-            product_value = int(input("Select a Product Menu option: "))
-            clear()
-            if product_value == 0:
-                print("Thank you for using the app!")
-                with open("products.txt", "w") as product_file:
-                    for product in products:
-                        product_file.write(product + "\n")
-                with open("couriers.txt", "w") as couriers_file:
-                    for courier in couriers:
-                        couriers_file.write(courier + "\n")
-                sys.exit(0)
-            elif product_value == 1:
-                print("Products:\n")
-                print(products)
-                print("\n")
-            elif product_value == 2:
-                proj_fun.add_product(products)
-            elif product_value == 3:
-                proj_fun.update_product(products)
-            elif product_value == 4:
-                proj_fun.delete_product(products)
-            elif product_value == 5:
-                # Exits the products menu and returns the user to the main menu
-                product_menu = False
-            else:
-                print("Sorry, this is not an option.\n")
-    elif value == 2:
-        courier_menu = True
-        while courier_menu == True:
-            print("Press... \n 0 to exit the app"
-                  + "\n 1 to see couriers" 
-                  + "\n 2 to add a new courier" 
-                  + "\n 3 to update an existing courier"
-                  + "\n 4 to delete a courier"
-                  + "\n 5 to return to Main Menu\n")
-            courier_value = int(input("Select a Courier Menu option: "))
-            clear()
-            if courier_value == 0:
-                print("Thank you for using the app!")
-                with open("products.txt", "w") as product_file:
-                    for product in products:
-                        product_file.write(product + "\n")
-                with open("couriers.txt", "w") as couriers_file:
-                    for courier in couriers:
-                        couriers_file.write(courier + "\n")
-                sys.exit(0)
-            elif courier_value == 1:
-                print("Couriers:\n")
-                print(couriers)
-                print("\n")
-            elif courier_value == 2:
-                proj_fun.add_courier(couriers)
-            elif courier_value == 3:
-                proj_fun.update_courier(couriers)
-            elif courier_value == 4:
-                proj_fun.delete_courier(couriers)
-            elif courier_value == 5:
-                # Exits the Courier Menu and returns user to the Main Menu
-                courier_menu = False
-            else:
-                print("Sorry, this is not an option.\n")
-    elif value == 3:
-        order_menu = True
-        while order_menu == True:
-            print("Press... \n 0 to exit the app"
-                  + "\n 1 to see orders"
-                  + "\n 2 to create new order"
-                  + "\n 3 to update order status"
-                  + "\n 4 to update order"
-                  + "\n 5 to delete order"
-                  + "\n 6 to return to Main Menu\n")
-            order_value = int(input("Select an Order Menu option: "))
-            clear()
-            if order_value == 0:
-                print("Thank you for using the app!")
-                with open("products.txt", "w") as product_file:
-                    for product in products:
-                        product_file.write(product + "\n")
-                with open("couriers.txt", "w") as couriers_file:
-                    for courier in couriers:
-                        couriers_file.write(courier + "\n")
+    if str_value.isdigit():
+        value = int(str_value)
+        if value == 0:
+            print("Thank you for using the app!")
+            with open('products.csv', 'w') as products_file:
+                for product_entry in products:
+                    fieldnames = ["Product", "Price"]
+                    writer = csv.DictWriter(products_file, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerow(product_entry.values())
+            with open("couriers.txt", "w") as couriers_file:
+                for courier in couriers:
+                    couriers_file.write(courier + "\n")
+            sys.exit(0)
+        elif value == 1:
+            product_menu = True
+            # This keeps the user in the products menu until they choose to leave.
+            while product_menu == True:
+                print("Press... \n 0 to exit the app"
+                    + "\n 1 to see products" 
+                    + "\n 2 to add a new product" 
+                    + "\n 3 to update an existing product"
+                    + "\n 4 to delete a product"
+                    + "\n 5 to return to Main Menu\n")
+                str_product_value = (input("Select a Product Menu option: "))
+                clear()
+                if str_product_value.isdigit():
+                    product_value = int(str_product_value)
+                    if product_value == 0:
+                        print("Thank you for using the app!")
+                        with open("products.txt", "w") as product_file:
+                            for product in products:
+                                product_file.write(product + "\n")
+                        with open("couriers.txt", "w") as couriers_file:
+                            for courier in couriers:
+                                couriers_file.write(courier + "\n")
                         sys.exit(0)
-            elif order_value ==1:
-                print("Orders:\n")
-                print(orders)
-                print("\n")
-            elif order_value == 2:
-                proj_fun.create_new_order(orders, couriers)
-            elif order_value == 3:
-                proj_fun.update_order_status(orders)
-            elif order_value == 4:
-                proj_fun.update_order(orders)
-            elif order_value == 5:
-                proj_fun.delete_order(orders)
-            elif order_value == 6:
-                order_menu = False
-            else:
-                print("Sorry this is not an option.\n")
-
-
-
-#When exiting the app from the orders menu, couriers list is reduced to only the first entry
+                    elif product_value == 1:
+                        print("Products:\n")
+                        print(products)
+                        print("\n")
+                    elif product_value == 2:
+                        proj_fun.add_product(products)
+                    elif product_value == 3:
+                        proj_fun.update_product(products)
+                    elif product_value == 4:
+                        proj_fun.delete_product(products)
+                    elif product_value == 5:
+                        # Exits the products menu and returns the user to the main menu
+                        product_menu = False
+                    else:
+                        print("\nSorry, this is not an option.\n")
+                else:
+                    print("\n Sorry, input needs to be an integer\n")
+        elif value == 2:
+            courier_menu = True
+            while courier_menu == True:
+                print("Press... \n 0 to exit the app"
+                    + "\n 1 to see couriers" 
+                    + "\n 2 to add a new courier" 
+                    + "\n 3 to update an existing courier"
+                    + "\n 4 to delete a courier"
+                    + "\n 5 to return to Main Menu\n")
+                str_courier_value = (input("Select a Courier Menu option: "))
+                clear()
+                if str_courier_value.isdigit():
+                    courier_value = int(str_courier_value)
+                    if courier_value == 0:
+                        print("Thank you for using the app!")
+                        with open("products.txt", "w") as product_file:
+                            for product in products:
+                                product_file.write(product + "\n")
+                        with open("couriers.txt", "w") as couriers_file:
+                            for courier in couriers:
+                                couriers_file.write(courier + "\n")
+                        sys.exit(0)
+                    elif courier_value == 1:
+                        print("Couriers:\n")
+                        print(couriers)
+                        print("\n")
+                    elif courier_value == 2:
+                        proj_fun.add_courier(couriers)
+                    elif courier_value == 3:
+                        proj_fun.update_courier(couriers)
+                    elif courier_value == 4:
+                        proj_fun.delete_courier(couriers)
+                    elif courier_value == 5:
+                        # Exits the Courier Menu and returns user to the Main Menu
+                        courier_menu = False
+                    else:
+                        print("\nSorry, this is not an option.\n")
+                else:
+                    print("\nSorry, input needs to be an integer\n")
+        elif value == 3:
+            order_menu = True
+            while order_menu == True:
+                print("Press... \n 0 to exit the app"
+                    + "\n 1 to see orders"
+                    + "\n 2 to create new order"
+                    + "\n 3 to update order status"
+                    + "\n 4 to update order"
+                    + "\n 5 to delete order"
+                    + "\n 6 to return to Main Menu\n")
+                str_order_value = (input("Select an Order Menu option: "))
+                clear()
+                if str_order_value.isdigit():
+                    order_value = int(str_order_value)
+                    if order_value == 0:
+                        print("Thank you for using the app!")
+                        with open("products.txt", "w") as product_file:
+                            for product in products:
+                                product_file.write(product + "\n")
+                        with open("couriers.txt", "w") as couriers_file:
+                            for courier in couriers:
+                                couriers_file.write(courier + "\n")
+                        sys.exit(0)
+                    elif order_value ==1:
+                        print("Orders:\n")
+                        print(orders)
+                        print("\n")
+                    elif order_value == 2:
+                        proj_fun.create_new_order(orders, couriers)
+                    elif order_value == 3:
+                        proj_fun.update_order_status(orders)
+                    elif order_value == 4:
+                        proj_fun.update_order(orders)
+                    elif order_value == 5:
+                        proj_fun.delete_order(orders)
+                    elif order_value == 6:
+                        order_menu = False
+                    else:
+                        print("\nSorry this is not an option.\n")
+                else:
+                    print("\nSorry, input needs to be an integer\n")
+    else:
+        print("\nSorry, input needs to be an integer\n")
--- proj_fun.py
+++ proj_fun.py
@@ -1,126 +1,38 @@
 def add_product(product_list):
+    new_product_entry = {}
     new_product = input("What product are you adding? Press 0 to cancel. ")
+    product_price = float(input(f"What is the price of {new_product}? "))
     if new_product.strip() == "0":
         return
     else:
-        product_list.append(new_product.title().strip())
+        new_product_entry["Product"] = new_product.title().strip()
+        new_product_entry["Price"] = product_price
+        product_list.append(new_product_entry)
         print("Products:\n")
         print(product_list)
         print("\n")
-    
-    
 
-
-def delete_product(product_list):
-        product = input("Which product would you like to delete? Press 0 to cancel. ")
-        if product.title().strip() in product_list:
-            product_index = product_list.index(product.title().strip())
-            product_list.pop(product_index)
-            print("Products:\n")
-            print(product_list)
-            print("\n")
-            return product_list
-        elif product.strip() == "0":
-            return
-        else:
-            print("Sorry, we do not have this product in stock")
-        
-    
-def add_courier(courier_list):
-    new_courier = input("Which courier are you adding? Press 0 to cancel. ")
-    if new_courier.strip() == "0":
-        return
-    else:
-        courier_list.append(new_courier.title().strip())
-        print("Couriers:\n")
-        print(courier_list)
-        print("\n")
-        return courier_list
-    
-    
-def update_courier(courier_list):
-    courier = input("Which courier would you like to update? Press 0 to cancel. ")
-    if courier.title().strip() in courier_list:
-        updated_courier = input("What would you like to update it to? ")
-        courier_index = courier_list.index(courier.title().strip())
-        courier_list[courier_index] = updated_courier.title().strip()
-        print("Couriers:\n")
-        print(courier_list)
-        print("\n")
-        return courier_list
-    elif courier.strip() == "0":
-        return
-    else:
-        print("Sorry, we do not have this courier in our database")
-              
-              
-def delete_courier(courier_list):
-    courier = input("Which courier would you like to delete? Press 0 to cancel. ")
-    if courier.title().strip() in courier_list:
-        courier_index = courier_list.index(courier.title().strip())
-        courier_list.pop(courier_index)
-        print("Couriers:\n")
-        print(courier_list)
-        print("\n")
-        return courier_list
-    elif courier.strip() == "0":
-        return
-    else:
-        print("Sorry, we do not have this courier in our database")
-        
-
-def create_new_order(order_list, courier_list):
-    new_order = {}
-    name = input("What is the name of the customer? ")
-    address = input("What is the customers address? ")
-    phone = input("What is the customers phone number? ")
-    status = "Preparing"
-    print("Couriers:\n")
-    print(courier_list)
-    print("\n")
-    courier = input("Which courier would you like? ")
-    if courier.title().strip() in courier_list:
-        new_order["Name"] = name.title().strip()
-        new_order["Address"] = address.title().strip()
-        new_order["Phone Number"] = phone.strip()
-        new_order["Status"] = status
-        order_list.append(new_order)
-        return order_list
-    else:
-        print("Sorry, we do not have this courier in our database")
 
 def update_product(product_list):
-    product = input("Which product would you like to update? Press 0 to cancel. ")
-    if product.title().strip() in product_list:
-        updated_product = input("What would you like to update it to? ")
-        product_index = product_list.index(product.title().strip())
-        product_list[product_index] = updated_product.title().strip()
-        print("Products:\n")
-        print(product_list)
-        print("\n")
-        return product_list
-    elif product.strip() == "0":
+    product_update = input("Which product would you like to update? Press 0 to cancel. ")
+    if product_update.strip() == "0":
         return
     else:
-        print("Sorry, we do not have this product in stock")
-      
-        
-# filter(lambda person: person['name'] == 'Pam', people)
-
-def update_order_status(order_list):
-    order_name = input("Whose order would you like to update the status of? Press 0 to cancel. ")
-    if order_name.strip() == "0":
-        return
-    else:
-        for order in order_list:
-            if order["Name"] == order_name.title().strip():
-                new_status = input("What is the new order status? ")
-                order["Status"] = new_status.title().strip()
-                print(order)
-                return order_list
+        for product in product_list:
+            if product["Product"] == product_update.title().strip():
+                new_product = input("What would you like to update the product to? Leave blank to skip. ")
+                if new_product != "":
+                    product["Product"] = new_product.title().strip()
+                new_price = float(input("What would you like to update the price to? Leave blank to skip. "))
+                if new_price != "":
+                    product["Price"] = new_price
+                print("Products:\n")
+                print(product_list)
+                print("\n")
+                return product_list
             else:
                 continue
-        print("Sorry, we do not have an order under this name.\n")
+        print("Sorry, we do not have this product in stock")
 
 def update_order(order_list):
     order_name = input("Whose order would you like to update? Press 0 to cancel. ")
@@ -142,6 +54,103 @@ def update_order(order_list):
             else:
                 continue
         print("Sorry, we do not have an order under this name.\n")
+
+def delete_product(product_list):
+        product = input("Which product would you like to delete? Press 0 to cancel. ")
+        if product.title().strip() in product_list:
+            product_index = product_list.index(product.title().strip())
+            product_list.pop(product_index)
+            print("Products:\n")
+            print(product_list)
+            print("\n")
+            return product_list
+        elif product.strip() == "0":
+            return
+        else:
+            print("Sorry, we do not have this product in stock")
+
+
+def add_courier(courier_list):
+    new_courier = input("Which courier are you adding? Press 0 to cancel. ")
+    if new_courier.strip() == "0":
+        return
+    else:
+        courier_list.append(new_courier.title().strip())
+        print("Couriers:\n")
+        print(courier_list)
+        print("\n")
+        return courier_list
+
+
+def update_courier(courier_list):
+    courier = input("Which courier would you like to update? Press 0 to cancel. ")
+    if courier.title().strip() in courier_list:
+        updated_courier = input("What would you like to update it to? ")
+        courier_index = courier_list.index(courier.title().strip())
+        courier_list[courier_index] = updated_courier.title().strip()
+        print("Couriers:\n")
+        print(courier_list)
+        print("\n")
+        return courier_list
+    elif courier.strip() == "0":
+        return
+    else:
+        print("Sorry, we do not have this courier in our database")
+
+
+def delete_courier(courier_list):
+    courier = input("Which courier would you like to delete? Press 0 to cancel. ")
+    if courier.title().strip() in courier_list:
+        courier_index = courier_list.index(courier.title().strip())
+        courier_list.pop(courier_index)
+        print("Couriers:\n")
+        print(courier_list)
+        print("\n")
+        return courier_list
+    elif courier.strip() == "0":
+        return
+    else:
+        print("Sorry, we do not have this courier in our database")
+
+
+def create_new_order(order_list, courier_list):
+    new_order = {}
+    name = input("What is the name of the customer? ")
+    address = input("What is the customers address? ")
+    phone = input("What is the customers phone number? ")
+    status = "Preparing"
+    print("Couriers:\n")
+    print(courier_list)
+    print("\n")
+    courier = input("Which courier would you like? ")
+    if courier.title().strip() in courier_list:
+        new_order["Name"] = name.title().strip()
+        new_order["Address"] = address.title().strip()
+        new_order["Phone Number"] = phone.strip()
+        new_order["Status"] = status
+        order_list.append(new_order)
+        return order_list
+    else:
+        print("Sorry, we do not have this courier in our database")
+
+
+def update_order_status(order_list):
+    order_name = input("Whose order would you like to update the status of? Press 0 to cancel. ")
+    if order_name.strip() == "0":
+        return
+    else:
+        for order in order_list:
+            if order["Name"] == order_name.title().strip():
+                new_status = input("What is the new order status? ")
+                order["Status"] = new_status.title().strip()
+                print(order)
+                return order_list
+            else:
+                continue
+        print("Sorry, we do not have an order under this name.\n")
+
+
+
 
 def delete_order(order_list):
     order_name = input("Whose order would you like to delete? Press 0 to cancel. ")
